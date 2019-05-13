fe70fc03ae2fa7235983c1afe97bf98eaf00d7c0
Iris Wolfisberg <iris.wolfisberg@hotmail.fr>
2019-05-13 16:08:51 +0200

    Finished

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+Rails.application.config.session_store :cookie_store, key: :score
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
 Rails.application.config.action_dispatch.cookies_serializer = :json
